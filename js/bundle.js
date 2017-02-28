de2d67eaa4da079665fb81ae07077eb680f70885
Flubbex <michael_nijhof@live.com>
2017-02-28 15:25:09 +0100

    Cleaned everything. v0.1.0

--- js/bundle.js
+++ js/bundle.js
@@ -1,12 +1,31 @@
-function module(domid)
+config = {};
+config.version = '0.1.0';
+
+Object.dump = function(prop)
+{
+	var seen = [];
+	return JSON.stringify(prop,function(key, val) 
+			{
+			   if (val != null && typeof val == "object") 
+			   {
+					if (seen.indexOf(val) >= 0) 
+					{
+						return;
+					}
+					seen.push(val);
+				}
+				return val;
+			});								
+};
+	
+function Module(domid)
 {
 	this.elm = document.getElementById(domid);
-	if (this.elm === null)
+	if (!this.elm || this.elm === null)
 		throw new Error("Hey numbnuts, that element doesn't exist. (#"+domid+")");
 	
-}
-
-module.prototype.write = function()
+};
+Module.prototype.write = function()
 {
 	var args = [].slice.call(arguments);
 	var newelm = document.createElement("div");
@@ -14,64 +33,107 @@ module.prototype.write = function()
 	{
 		var prop = args[i]
 		var newspan = document.createElement("span");
-		newspan.innerHTML = typeof(prop)==='object' ? 
-								function(prop){
-									seen = [];
-									return JSON.stringify(prop,function(key, val) 
-											{
-											   if (val != null && typeof val == "object") 
-											   {
-													if (seen.indexOf(val) >= 0) 
-													{
-														return;
-													}
-													seen.push(val);
-												}
-												return val;
-											});
-									}(prop) : prop
+		var string;
+			switch (typeof(prop))
+			{
+				case "undefined":
+					string = "'undefined'";
+					newspan.style.color = "blue";
+					break;
+				case "object":
+					if (prop._iserror) //its an error
+						{
+							string = prop.toString();
+							newspan.style.color = 'red';
+						}
+					else
+						string = Object.dump(prop)
+					break;
+				case "string":
+				case "number":
+					string = prop;
+					break;
+				default:
+					this.write("Warning: Unhandled type cast to string: ",typeof(prop))
+					string = prop.toString();
+					break;
+			}
+		newspan.innerHTML = string;
 		newelm.appendChild(newspan);
 	}
 	this.elm.appendChild(newelm);
 	return newelm;
 };
-module.prototype.read = function(attr)
+Module.prototype.read = function(attr)
 {
 	return this.elm[attr];
 }
-module.prototype.on	=	function(event,callback)
+Module.prototype.on	=	function(event,callback)
 {
 	return this.elm.addEventListener(event,callback);
 }
-module.prototype.clear	= function()
+Module.prototype.clear	= function()
 {
 	this.elm.value = "";
 }
-
-
-var JSConsole = function(jsconsole,input,submit)
+Module.prototype.css = function(attr,value)
 {
-	function out()
-	{
-		var args = [].slice.call(arguments);
-		
-		var timestamp = function(d){
-			return d.getHours()+":"+
-				   d.getMinutes()+":"+
-				   d.getSeconds();
-		}(new Date());
-		
-		args.unshift(timestamp);
-		jsconsole.write.apply(jsconsole,
-							args).scrollIntoView();
-	}
+	this.elm.style[attr] = value;
+}
+
+function JSConsole(jsconsole,input,submit)
+{
+	this.console 	= jsconsole;
+	this.input 		= input;
+	this.submit 	= submit;
+}
+JSConsole.prototype.parseInput = function parseInput()
+{
+	var result = null;
+	try
+		{
+			result = eval(this.input.read("value"));
+			this.input.clear();
+			this.submit.css("color","green");
+		}
+	catch (e)
+		{
+			result = e;
+			result._iserror = true;
+			this.submit.css("color","red");
+			
+		}
+	this.log( result );
+}
+
+JSConsole.prototype.log = function out()
+{
 	
-	out("JSConsole running");
+	var args = [].slice.call(arguments);
+	var timestamp = function(d){
+		return d.getHours()+":"+
+			   d.getMinutes()+":"+
+			   d.getSeconds();
+	}(new Date());
 	
-	submit.on("click",function()
-	{
-		out( eval(input.read("value")) );
-		input.clear();
-	});
-		
-}(new module("console"),new module("text"),new module("submit"));
+	args.unshift(timestamp);
+	this.console.write.apply(this.console,
+						args).scrollIntoView();
+};
+
+(function initialise(){
+	
+	var jsc = new JSConsole(new Module("console"),
+								new Module("text"),
+								new Module("submit"));
+								
+	jsc.submit.on("click",this.parseInput);
+	jsc.input.on("keydown",function(e){
+		if (e.keyCode===13)
+			jsc.parseInput()
+	});	
+
+	window.addEventListener('error',function(e){jsc.log(e)});
+
+	jsc.log("JSConsole (",config.version,") running");
+})();
