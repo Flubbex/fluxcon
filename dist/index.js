571f45cdf58a0bf7d23ecefbccaf6806a7ba5083
Flubbex <michael_nijhof@live.com>
2017-03-07 14:44:30 +0100

    Minor layout update fix

--- dist/index.js
+++ dist/index.js
@@ -128,7 +128,7 @@ module.exports = View;
 //IDEA: Loaded controllers in config
 
 Config = {};
-Config.version = "0.2.5";
+Config.version = "0.2.5b";
 Config.vername = "Savage Spaghetti";
 Config.console = {};
 Config.console.init = "J0ZsdXhjb24gJytmbHV4Y29uc29sZS5jb25maWcudmVyc2lvbisnICgnK2ZsdXhjb25zb2xlLmNvbmZpZy52ZXJuYW1lKycpIHJ1bm5pbmcuJw==";
